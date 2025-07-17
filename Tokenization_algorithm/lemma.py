50aa424d082c4d1b64a16423aab71f1b8e819969
Sachin Chaudhary <sachin.chaudhary.ai@gmail.com>
2025-07-18 00:14:43 +0530

    Update lemma.py

--- Tokenization_algorithm/lemma.py
+++ Tokenization_algorithm/lemma.py
@@ -1,1 +1,12 @@
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
+
+def lemmatize(text):
+    doc = nlp(text)
+    return [token.lemma_ for token in doc]
+
+# Example:
+text = "Sachin studies NLP and loved learning new things"
+lemmas = lemmatize(text)
+print(lemmas) 
